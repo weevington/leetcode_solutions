dcd0214004a945f1317b28e5838756bbf97e4b91
Weevington <weevington@gmail.com>
2020-04-02 18:12:30 -0700

    Added problem 202 happy number

---
+++ P202HappyNumber/happy_number.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """
+        Parameters
+        ----------
+        n : [int]
+        Input integer.
+
+        Returns
+        -------
+        happy: bool
+        True if the number is a "happy" number.
+        """
+        happy = False
+
+        h = 0
+        num = n
+
+        while num > 0:
+            h += (num % 10)**2
+            num = num // 10
+            if num == 0:
+                if h == 1:
+                    happy = True
+                    break
+                elif h == 4:
+                    break
+                else:
+                    num = h
+                    h = 0
+
+        return happy
