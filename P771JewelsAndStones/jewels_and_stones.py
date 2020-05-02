10c2413aa244242c2ad439cbe97c563b73f86e9f
Weevington <weevington@gmail.com>
2020-05-02 10:09:27 -0700

    Fixed documentation for Problem 771 Jewels and Stones (part of LeetCode 30 day challenge May 2020.

--- P771JewelsAndStones/jewels_and_stones.py
+++ P771JewelsAndStones/jewels_and_stones.py
@@ -1,34 +1,34 @@
 class Solution:
-    """
-    You're given strings J representing the types of stones that are jewels, 
-    and S representing the stones you have.  Each character in S is a type of
-    stone you have. This function returns how many of the stones in a given 
-    collection are are also jewels.
-
-    Parameters
-    ----------
-    J : str
-        String representing jewels. Case sensitive, i.e. "aA" represents
-        two different stones
-    
-    S : str
-        String representing jewels. Case sensitive, i.e. "aA" represents
-        two different stones
-
-    Returns
-    -------
-    num_jewels : int
-        Number of jewels in the collection of stones.
-
-    Examples
-    --------
-    Input: J = "aA", S = "aAAbbbb"
-    Output: 3
-
-    Input: J = "z", S = "ZZ"
-    Output: 0
-    """
     def numJewelsInStones(self, J: str, S: str) -> int:
+        """
+        You're given strings J representing the types of stones that are jewels, 
+        and S representing the stones you have.  Each character in S is a type of
+        stone you have. This function returns how many of the stones in a given 
+        collection are are also jewels.
+
+        Parameters
+        ----------
+        J : str
+            String representing jewels. Case sensitive, i.e. "aA" represents
+            two different stones
+        
+        S : str
+            String representing jewels. Case sensitive, i.e. "aA" represents
+            two different stones
+
+        Returns
+        -------
+        num_jewels : int
+            Number of jewels in the collection of stones.
+
+        Examples
+        --------
+        Input: J = "aA", S = "aAAbbbb"
+        Output: 3
+
+        Input: J = "z", S = "ZZ"
+        Output: 0
+        """
         jewel_dict = {}
         
         for i, s in enumerate(S):
@@ -46,39 +46,39 @@ class Solution:
 
 
 class Solution:
-    """
-    You're given strings J representing the types of stones that are jewels, 
-    and S representing the stones you have.  Each character in S is a type of
-    stone you have. This function returns how many of the stones in a given 
-    collection are are also jewels.
-
-    This solution uses a set (hash set) to keep track of the jewels, i.e.,
-    there should only be one type of each jewel. It also makes use of the
-    sum() function for Python.
-
-    Parameters
-    ----------
-    J : str
-        String representing jewels. Case sensitive, i.e. "aA" represents
-        two different stones
-    
-    S : str
-        String representing jewels. Case sensitive, i.e. "aA" represents
-        two different stones
-
-    Returns
-    -------
-    num_jewels : int
-        Number of jewels in the collection of stones.
-
-    Examples
-    --------
-    Input: J = "aA", S = "aAAbbbb"
-    Output: 3
-
-    Input: J = "z", S = "ZZ"
-    Output: 0
-    """
     def numJewelsInStones(self, J, S):
+        """
+        You're given strings J representing the types of stones that are jewels, 
+        and S representing the stones you have.  Each character in S is a type of
+        stone you have. This function returns how many of the stones in a given 
+        collection are are also jewels.
+
+        This solution uses a set (hash set) to keep track of the jewels, i.e.,
+        there should only be one type of each jewel. It also makes use of the
+        sum() function for Python.
+
+        Parameters
+        ----------
+        J : str
+            String representing jewels. Case sensitive, i.e. "aA" represents
+            two different stones
+        
+        S : str
+            String representing jewels. Case sensitive, i.e. "aA" represents
+            two different stones
+
+        Returns
+        -------
+        num_jewels : int
+            Number of jewels in the collection of stones.
+
+        Examples
+        --------
+        Input: J = "aA", S = "aAAbbbb"
+        Output: 3
+
+        Input: J = "z", S = "ZZ"
+        Output: 0
+        """
         Jset = set(J)
         return sum(s in Jset for s in S)
