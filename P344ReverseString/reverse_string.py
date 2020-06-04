f6a0e3822115140e22e7f439737d9493a2ba6d1e
Weevington <weevington@gmail.com>
2020-06-04 08:40:00 -0700

    Added reverse string in Python3 with detailed explanation of the method.

---
+++ P344ReverseString/reverse_string.py
@@ -0,0 +1,51 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Reverses a string represented as a list of characters
+        in-place. No function methods from the list class like reverse() 
+        are used.
+        
+        This approach uses two pointers (not literal pointers in the 
+        C/C++ sense), one for the front (lo), and one that iterates 
+        from the end (hi). The characters at positions lo and hi are
+        swapped and . The running time is O(n) and the space complexity
+        is constant O(1).
+        
+        Consider the example "hello, world". 
+
+        "hello, world"   -->   "dello, worlh"  -->   "dello, worlh" 
+         ^          ^           ^          ^           ^        ^
+        lo          hi          lo         hi          lo       hi
+
+        "dello, worlh"   -->   "dlllo, woreh"  -->   "dlllo, woreh" 
+          ^        ^             ^        ^             ^      ^
+          lo       hi            lo       hi            lo     hi
+        
+        "dlllo, woreh"   -->   "dlrlo, woleh"  -->   "dlrlo, woleh" 
+           ^      ^               ^      ^               ^    ^   
+           lo     hi              lo     hi              lo   hi
+        
+        ...
+        ...
+
+        Parameters
+        ----------
+        s: List
+            List of characters to be reversed in-place
+    
+        Examples
+        --------
+        Input: ["H","a","n","n","a","h"]
+        Output: ["h","a","n","n","a","H"]
+
+        Input: ["w","i","n","t","e","r"]
+        Output ["r","e","t","n","i","w"]]
+        """
+        lo = 0
+        hi = len(s) - 1
+        
+        while lo < hi:
+            s[lo], s[hi] = s[hi], s[lo]
+            
+            lo += 1
+            hi -= 1
