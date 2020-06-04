4f87d52889b2f7dc10b8074b27d78443eca99665
Weevington <weevington@gmail.com>
2020-06-04 09:09:58 -0700

    Additional documentation for Problem 344 Reverse String in Python3.

--- P344ReverseString/reverse_string.py
+++ P344ReverseString/reverse_string.py
@@ -10,8 +10,10 @@ class Solution:
         from the end (hi). The characters at positions lo and hi are
         swapped, the lo pointer is incremeneted to the next character,
         while the hi pointer is decremented to the character before the
-        current hi position. The time complexity is O(n) and the space
-        complexity is constant O(1).
+        current hi position. The algorithm terminates once lo is equal to hi
+        (in the case of a string with odd length) or greater than hi (in the
+        case of a string of even length). The time complexity is O(n) and 
+        the space complexity is constant O(1).
         
         Consider the example "hello, world". 
 
